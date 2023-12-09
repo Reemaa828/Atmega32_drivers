ba7d0ba9430657487afd0f43aa3d0dbc0b06bda4
Reemaa828 <reemoto51@gmail.com>
2023-12-09 13:17:08 +0200

    change in functions for reusability

--- HAL/LED/LED_program.c
+++ HAL/LED/LED_program.c
@@ -6,23 +6,28 @@
 #include "LED_private.h"
 
 void LED_voidInit(LED_Config LED){
-    DIO_voidSetPinDirection(LED.port,LED.pin,output);
+    DIO_voidSetPinDirection(LED.port,LED.pin,DIO_output);
 }
 
 void LED_voidTurnOn(LED_Config LED){
     if(LED.connection==LED_sink){
-    DIO_voidSetPinValue(LED.port,LED.pin,low);
+    DIO_voidSetPinValue(LED.port,LED.pin,DIO_low);
     }
     else if(LED.connection==LED_source){
-        DIO_voidSetPinValue(LED.port,LED.pin,high);
+        DIO_voidSetPinValue(LED.port,LED.pin,DIO_high);
     }
 }
 
 void LED_voidTurnOff(LED_Config LED){
     if(LED.connection==LED_sink){
-    DIO_voidSetPinValue(LED.port,LED.pin,high);
+    DIO_voidSetPinValue(LED.port,LED.pin,DIO_high);
     }
     else if(LED.connection==LED_source){
-        DIO_voidSetPinValue(LED.port,LED.pin,low);
+        DIO_voidSetPinValue(LED.port,LED.pin,DIO_low);
     }
 }
+
+void LED_voidToggle(LED_Config LED){
+    DIO_voidTogglePin(LED.port,LED.pin);
+}
+
