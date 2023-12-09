4c877d610e110f312a9a625593995cf71afb5b70
Reemaa828 <reemoto51@gmail.com>
2023-12-09 13:20:31 +0200

    naming change

--- MCAL/DIO/DIO_interface.h
+++ MCAL/DIO/DIO_interface.h
@@ -2,37 +2,42 @@
 #define _DIO_INTERFACE_H
 
 typedef enum {
-    allinput=0x00,
-    alloutput=0xff
+    DIO_allinput=0x00,
+    DIO_alloutput=0xff
 } DIO_PortDirection;
 
 
 typedef enum{
-    input,
-    output
+    DIO_input,
+    DIO_output
 }DIO_PinDirection;
 
 typedef enum {
-    Pin0,
-    Pin1,
-    Pin2,
-    Pin3,
-    Pin4,
-    Pin5,
-    Pin6,
-    Pin7,
+    DIO_pin0,
+    DIO_pin1,
+    DIO_pin2,
+    DIO_pin3,
+    DIO_pin4,
+    DIO_pin5,
+    DIO_pin6,
+    DIO_pin7,
 } DIO_PinNumber;
 
 typedef enum{
-    low,
-    high
+    DIO_low,
+    DIO_high
 }DIO_PinValue;
+typedef enum{
+    DIO_alllow=0x00,
+    DIO_allhigh=0xff
+}DIO_PortValue;
 
 void DIO_voidSetPortDirection(u8  Copy_u8Port, DIO_PortDirection direction);
-void DIO_voidSetPortValue(u8 Copy_u8Port,u8 Copy_u8Value);
+void DIO_voidSetPortValue(u8 Copy_u8Port,DIO_PortValue value);
 void DIO_voidSetPinDirection(u8 Copy_u8Port,DIO_PinNumber pin,DIO_PinDirection direction);
 void DIO_voidSetPinValue(u8 Copy_u8Port,DIO_PinNumber pin,DIO_PinValue value);
 u8   DIO_u8GetPinValue(u8 Copy_u8Port,DIO_PinNumber pin);
 void DIO_voidEnablePullupResistor(u8 Copy_u8Port,DIO_PinNumber pin);
+void DIO_voidTogglePin(u8 Copy_u8Port,DIO_PinNumber pin);
 
 #endif
